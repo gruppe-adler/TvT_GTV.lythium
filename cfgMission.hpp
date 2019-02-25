25ca07113d64726c7ad6623ff13d71db58d4c7b5
McDiod <hobbeldobbel@gmail.com>
2019-02-25 17:56:04 +0100

    reset objectives timer values

--- cfgMission.hpp
+++ cfgMission.hpp
@@ -2,9 +2,9 @@ class cfgMission {
     canTakeEnemyRadios = 0;                                                     // Können Spieler feindliche Funkgeräte aufsammeln? (0/1)
     canUseScopes = 1;                                                           // Können Spieler vergrößernde Visiere auf allen Waffen benutzen? (0/1) - siehe cfgScopeWeapons.hpp
 
-    vipObjectiveTimeoutDefault = 60;                                            // Zeit in Sekunden, die der VIP in einem Objective stehen muss, um es zu erfüllen. Solange nicht von Funktion überschrieben.
+    vipObjectiveTimeoutDefault = 150;                                           // Zeit in Sekunden, die der VIP in einem Objective stehen muss, um es zu erfüllen. Solange nicht von Funktion überschrieben.
     vipObjectiveTimeoutCancel = 10;                                             // Zeit in Sekunden, die der VIP außerhalb der Objective Zone verbringen kann, bevor die Zeit zurückgesetzt wird.
-    vipBaseTaskTimeout = 10;                                                   // Zeit in Sekunden, die der VIP zum Schluss der Mission in seiner Basis verbringen muss, bevor Blufor gewinnt.
+    vipBaseTaskTimeout = 300;                                                   // Zeit in Sekunden, die der VIP zum Schluss der Mission in seiner Basis verbringen muss, bevor Blufor gewinnt.
 
     vipObjectiveShotsDetectionRadius = 250;                                     // Entfernung in Metern, in der Opfor-Schüsse zum Taskabbruch führen
     vipObjectiveShotsAmountForCancel = 3;                                       // Anzahl an Schüssen, die Opfor abgeben muss, damit der Task abgebrochen wird.
